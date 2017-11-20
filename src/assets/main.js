14bd40d81e490f98b2bec9efc01eb3eaf5c3c9c3
Amit Kumar (amie) <amitkm040@gmail.com>
2017-11-20 13:07:02 +0530

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,9 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if(answer.value == "" || answer.value == "") {
+        setHiddenFields();
+    }
 }
 
 //implement new functions here
@@ -13,6 +16,7 @@ answer.value = Math.floor(Math.random() * 10000).toString();
     while(answer.value.length < 4){
         answer.value = "0" + answer.value;
     }
+    attempt.value="0";
 }
 
 function setMessage(Textmessage) {
